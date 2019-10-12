9c45458999d27b4b7cca56db2b06f333f67a76ac
Andrew Wong <andrew.j.wong@outlook.com>
2019-10-12 16:15:15 +1100

    Spacing

--- src/App.svelte
+++ src/App.svelte
@@ -4,23 +4,36 @@
   import Filmstrip from "./Filmstrip.svelte";
 
   import MailLoader from "./MailLoader.svelte";
-  let showIntro = true;
+
+  let showIntro = false;
 
   function doHideIntro() {
-	  showIntro = false;
+    showIntro = false;
   }
 </script>
 
 <style>
+  .overflowContainer {
+    overflow-y: hidden;
+    height: 100%;
 
+flex-basis: 200px;
+    display: flex;
+  }
+  
+  :global(.overflowContainer > *) {
+    flex-shrink: 0;   
+  }
 </style>
-
-
 
 {#if showIntro}
   <MailLoader on:destroy={doHideIntro} />
 {:else}
-<Filmstrip />
+  <div class="overflowContainer">
+    <Filmstrip rotation="20" />
+    <Filmstrip rotation="-8" />
+    <Filmstrip rotation="3" />
+    <Filmstrip rotation="-1" />
 
-<Filmstrip />
+  </div>
 {/if}
--- src/Filmstrip.svelte
+++ src/Filmstrip.svelte
@@ -2,18 +2,20 @@
   import Tile from "./Tile.svelte";
   import { fade } from "svelte/transition";
   let items = [1, 2, 3, 4, 5, 6];
+
+  export let rotation = 0;
+
 </script>
 
 <style>
   .filmstrip {
-    float: left;
     margin-right: 30px;
     margin-bottom: 30px;
 
     width: 200px;
     background-color: rgb(241, 240, 232);
     height: 100vh;
-    transform: rotate(-15deg);
+    /* transform: rotate(-15deg); */
     overflow-y: scroll;
     padding: 15px;
 
@@ -27,7 +29,10 @@
   }
 </style>
 
-<div class="filmstrip" transition:fade={{ duration: 500 }}>
+<div
+  class="filmstrip"
+  transition:fade={{ duration: 500 }}
+  style={`transform: rotate(${rotation}deg); margin-right: ${200 * Math.abs(Math.sin(Math.abs(rotation)))}px`}>
   {#each items as item}
     <div class="spacer">
       <Tile />
