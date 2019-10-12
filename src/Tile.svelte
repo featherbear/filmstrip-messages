818fad05cb3214462d5d1690504791e8187ee94c
Andrew Wong <andrew.j.wong@outlook.com>
2019-10-12 15:41:53 +1100

    Tile hover effect

--- src/Tile.svelte
+++ src/Tile.svelte
@@ -1,12 +1,22 @@
 <style>
-div {
+.tile {
     width: 100%;
     min-height: 150px;
     background-color: red;
     cursor: pointer;
+
+    transform: scale(0.98);
+    transition: transform 0.2s;
+
+    text-align: center;
+    border-radius: 1px;
+}
+
+.tile:hover {
+    transform: scale(1.0);
 }
 
 </style>
-<div>
+<div class="tile">
 B
 </div>
