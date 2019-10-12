41d944025bfb9c62c3faded1b72bd804e501256d
Andrew Wong <andrew.j.wong@outlook.com>
2019-10-12 15:35:22 +1100

    Update mail loader

--- src/MailLoader.svelte
+++ src/MailLoader.svelte
@@ -18,7 +18,7 @@
     height: 100%;
     z-index: 30;
 
-    background-color: white;
+    background-color: rgba(0, 0, 0, 0.6);
 
     display: flex;
     align-items: center;
@@ -27,29 +27,70 @@
     text-align: center;
   }
 
-  .mail {
-    background-color: red;
+  .centreStyle {
     width: 90%;
     height: 70%;
+    max-width: 1024px;
+  }
+  .mail {
+    background-color: #fdf9df;
 
     display: flex;
     align-items: center;
     justify-content: center;
 
-    border-radius: 8xpx;
+    border-radius: 8px;
+  }
+
+  .lineContainer {
+    position: absolute;
+  }
+  .lineContainer > div {
+    height: 100%;
+    border-radius: 8px;
+
+    position: absolute;
+    top: 0;
+    width: 100%;
+  }
+
+  .lineContainer .lineOne {
+    background: linear-gradient(
+      to bottom right,
+      transparent calc(50% - 1px),
+      #cfcecb,
+      transparent calc(50% + 1px)
+    );
+  }
+  .lineContainer .lineTwo {
+    background: linear-gradient(
+      to top right,
+      transparent calc(50% - 1px),
+      #cfcecb,
+      transparent calc(50% + 1px)
+    );
+  }
+
+  .content {
+    z-index: 1;
   }
 
   button {
     cursor: pointer;
+    font-size: 1.5em;
+    border-radius: 8px;
   }
 </style>
 
 <div class="mailContainer" transition:fade={{ duration: 300 }}>
-  <div class="mail">
-    <div>
-      <p>Hey :)</p>
-      <button on:click={close}>Close</button>
+  <div class="mail centreStyle">
+    <div class="content">
+      <button on:click={close}>Open</button>
+    </div>
+
+    <div class="lineContainer centreStyle">
+      <div class="lineOne" />
+      <div class="lineTwo" />
     </div>
   </div>
-
 </div>
